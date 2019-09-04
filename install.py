8dd5a36195f9e47f177358d3d6e04d8c69cf73de
Aitor Arjona <aitor.a98@gmail.com>
2019-09-04 12:16:54 +0200

    Fixed install script errors

--- install.py
+++ install.py
@@ -9,19 +9,19 @@ gcp_functions_backend_path = os.path.join(os.path.join(base_path, 'compute', 'ba
 gcp_storage_backend_path = os.path.join(os.path.join(base_path, 'storage', 'backends'), 'gcp_storage')
 
 try:
-    if os.path.exists(aws_cb_backend_path) and os.path.isfile(gcp_functions_backend_path):
-        os.remove(aws_cb_backend_path)
-    if os.path.exists(aws_s3_backend_path) and os.path.isfile(gcp_storage_backend_path):
-        os.remove(aws_s3_backend_path)
+    if os.path.exists(gcp_functions_backend_path) and os.path.isfile(gcp_functions_backend_path):
+        os.remove(gcp_functions_backend_path)
+    if os.path.exists(gcp_storage_backend_path) and os.path.isfile(gcp_storage_backend_path):
+        os.remove(gcp_storage_backend_path)
 
-    if os.path.exists(aws_cb_backend_path) and os.path.isdir(gcp_functions_backend_path):
-        shutil.rmtree(aws_cb_backend_path)
-    if os.path.exists(aws_s3_backend_path) and os.path.isdir(gcp_storage_backend_path):
-        shutil.rmtree(aws_s3_backend_path)
+    if os.path.exists(gcp_functions_backend_path) and os.path.isdir(gcp_functions_backend_path):
+        shutil.rmtree(gcp_functions_backend_path)
+    if os.path.exists(gcp_storage_backend_path) and os.path.isdir(gcp_storage_backend_path):
+        shutil.rmtree(gcp_storage_backend_path)
 
-    if not os.path.exists(aws_s3_backend_path):
+    if not os.path.exists(gcp_functions_backend_path):
         shutil.copytree(os.path.join(source_path, 'compute_backend'), gcp_functions_backend_path)
-    if not os.path.exists(aws_s3_backend_path):
+    if not os.path.exists(gcp_storage_backend_path):
         shutil.copytree(os.path.join(source_path, 'storage_backend'), gcp_storage_backend_path)
 
     print('Done')
